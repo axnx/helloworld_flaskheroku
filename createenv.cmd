e768944ba265d66b2d097d34f803a2b34b9326b3
mstorost <info@storost.de>
2020-10-25 10:14:47 +0100

    neu

---
+++ createenv.cmd
@@ -0,0 +1,4 @@
+ cd C:\Users\User\Desktop\test2\helloworldstochie
+ conda env create --prefix=./env379hw -f environment.yml
+ #conda env create -f environment.yml
+ conda activate C:\Users\User\Desktop\test2\helloworldstochie\env379hw
